a4325ff2d52083fc63adcda0bde59ff655394bad
Brandon <brandon121j@DESKTOP-DPFP3JR.localdomain>
2021-09-14 12:34:16 -0400

    Addition of public folder and games.html

--- app.js
+++ app.js
@@ -6,11 +6,24 @@ const port = 3000;
 //     response.end("hi, class");
 // });
 
+// const server = http.createServer(function (request, response) {
+//     response.setHeader("Content-Type", "application/json");
+//     response.end(JSON.stringify({ text: "hello classes thing person", numbers: [1, 2, 3] }))
+// })
+
+// server.listen(port);
+
+// console.log(`Server is now up and running @ port: ${port}`)
+
 const server = http.createServer(function (request, response) {
-    response.setHeader("Content-Type", "application/json");
-    response.end(JSON.stringify({ text: "hello class", numbers: [1, 2, 3] }))
+    console.log(request.url);
+
+    if (request.url === "/games") {
+        response.end("check back later for game details");
+    } else {
+        response.end()
+    }
 })
 
 server.listen(port);
-
-console.log(`Server is now up and running @ port: ${port}`)
+console.log(`Server is now up and running @ port: ${port}`);
